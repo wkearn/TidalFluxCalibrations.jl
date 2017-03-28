9b2618f8fbb53de0eb71f955cefcd86501d09e6d
Will Kearney <wkearn@gmail.com>
2017-03-28 10:26:42 -0400

    Add tests. Closes #2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
-using TidalFluxCalibrations
+using DischargeData, PIEMetData, TidalFluxExampleData, ADCPDataProcessing, TidalFluxCalibrations
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+setADCPdatadir!(Pkg.dir("TidalFluxExampleData","data","adcp"))
+setmetdatadir!(Pkg.dir("TidalFluxExampleData","data","met"))
+
+creek = Creek{:sweeney}()
+deps = parse_deps(creek)
+adata = load_data.(deps)
+
+cs = parse_cs(creek)
+csd = load_data(cs)
+
+dd = Discharge(adata[4],csd)
+
+cals = parse_cals(creek)
+cc = load_data(cals[1])
+
+calibrateData([cc],dd,1)
