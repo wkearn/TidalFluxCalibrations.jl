6b3ef141e03b5ec53bda6f96b2d636476646e5a2
Will Kearney <wkearn@gmail.com>
2017-03-28 10:26:32 -0400

    CalibrationData -> Calibration

--- src/TidalFluxCalibrations.jl
+++ src/TidalFluxCalibrations.jl
@@ -4,7 +4,7 @@ export calibratePolynomial, calibrateData
 
 using DischargeData, Interpolations
 
-function interpolateCalibration(cal::CalibrationData)
+function interpolateCalibration(cal::Calibration)
     Qi = interpolate((cal.dd.ts,),cal.dd.Q,Gridded(Linear()))
     Qs = Qi[cal.t]
 end
@@ -13,7 +13,7 @@ end
 Constructs a polynomial regression of order k to calibrate 
 ADCP discharge to the true discharge
 """
-function calibratePolynomial(cal::CalibrationData,k)
+function calibratePolynomial(cal::Calibration,k)
     Qs = interpolateCalibration(cal)
     X = ones(length(Qs),k+1)
     for i in 1:k+1
@@ -23,9 +23,9 @@ function calibratePolynomial(cal::CalibrationData,k)
 end
 
 """
-Performs a global calibration for multiple CalibrationDatas
+Performs a global calibration for multiple Calibrations
 """
-function calibratePolynomial(cals::Vector{CalibrationData},k)
+function calibratePolynomial(cals::Vector{Calibration},k)
     Qs = vcat(interpolateCalibration.(cals)...)
     Qq = vcat([cals[i].Q for i in 1:length(cals)]...)
     X = ones(length(Qs),k+1)
@@ -44,12 +44,12 @@ function calibrateData(dd::Discharge,β::Vector{Float64})
     Discharge(dd.cp,dd.ts,dd.vs,dd.A,X*β)
 end
 
-function calibrateData(cal::CalibrationData,dd::Discharge,k::Int)
+function calibrateData(cal::Calibration,dd::Discharge,k::Int)
     β = calibratePolynomial(cal,k)
     calibrateData(dd,β)
 end
 
-function calibrateData(cals::Vector{CalibrationData},dd::Discharge,k::Int)
+function calibrateData(cals::Vector{Calibration},dd::Discharge,k::Int)
     β = calibratePolynomial(cals,k)
     calibrateData(dd,β)
 end
