e1f30dbbde45270f2211aeb35d6eaf8cdf909132
Will Kearney <wkearn@gmail.com>
2017-06-23 11:54:46 -0400

    Move Calibration typedefs to TFC

---
+++ src/calibrations.jl
@@ -0,0 +1,26 @@
+"""
+A Calibration holds two TidalFluxQuantities
+
+- qto is the 'true' quantity that we want to calibrate to
+- qfrom is the measured quantity that we want to calibrate from
+"""
+type Calibration{T<:Quantity, F<:Quantity}
+    qto::T
+    qfrom::F
+end
+
+"""Return the to quantity"""
+to_quantity(c::Calibration) = c.qto
+
+"""Return the from quantity"""
+from_quantity(c::Calibration) = c.qfrom
+
+DischargeData.times(c::Calibration) = (times(to_quantity(c)),times(from_quantity(c)))
+
+Base.size(c::Calibration) = (length(to_quantity(c)),length(from_quantity(c)))
+
+function Base.vcat{T,F}(cs::Calibration{T,F}...)    
+    qto = vcat(T[to_quantity(c) for c in cs]...)
+    qfrom = vcat(F[from_quantity(c) for c in cs]...)
+    Calibration(qto,qfrom)
+end
